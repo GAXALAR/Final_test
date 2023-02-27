2c14fb709fe4c620e5013e1b94b6ee841817b37a
GAXALAR <ay.kosyrev@gmail.com>
2023-02-27 16:00:01 +0300

    создали новый метод ArrayRes, который сортирует введенный ранее массив

--- итоговая проверочная/verif_work.cs
+++ итоговая проверочная/verif_work.cs
@@ -1,3 +1,15 @@
 ﻿Console.WriteLine("Введите текст: ");
 string textByUser = Console.ReadLine();
 string [] textAsArray = textByUser.Split(' ');
+
+
+void ArrayRes ( string [] textAsArray )
+{
+    for (int i = 0; i < textAsArray.Length; i++)
+    {
+        if (textAsArray[i].Length <= 3)
+        {
+            Console.Write ($"{textAsArray[i]} ");
+        }
+    }
+}
